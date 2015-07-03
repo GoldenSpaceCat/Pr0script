f59c1b498d02bc042a81b88749ef42e67e0422c6
Jan-Stefan Janetzky <git@gottz.de>
2015-07-03 16:33:19 +0200

    initial commit

---
+++ Pr0script.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Pr0script
+// @namespace    https://pr0gramm.com/
+// @version      0.1
+// @description  benis
+// @author       gwz, flummi
+// @match        http://pr0gramm.com/*
+// @match        https://pr0gramm.com/*
+// @updateURL    https://raw.githubusercontent.com/GoldenSpaceCat/Pr0script/raw/master/Pr0script.user.js
+// @downloadURL  https://raw.githubusercontent.com/GoldenSpaceCat/Pr0script/raw/master/Pr0script.user.js
+// @grant        unsafeWindow
+// ==/UserScript==
+
+var document = unsafeWindow.document;
+
+var head = document.querySelector("head");
+
+var style = document.createElement("style");
+
+var css = "\
+.comments .score:before {\
+    content: attr(title) \", \";\
+}\
+.item-vote .score {\
+    position: absolute;\
+    font-size: 32%;\
+    top: 50px;\
+    width: 100px;\
+    content: attr(title);\
+.item-vote .score:before {\
+    position: absolute;\
+    font-size: 32%;\
+    top: 50px;\
+    width: 100px;\
+    content: attr(title);\
+}";
+
+style.appendChild(document.createTextNode(css));
+
+head.appendChild(style);
